a43793b3f8fd87175ff479be8f051d1b7a6f8621
akshat2412 <Akshat.Gupta@blackbaud.me>
2020-03-31 02:08:11 +0530

    changed axios to got for making requests

--- Aggregator_Microservice/src/Shared/Services/api.service.ts
+++ Aggregator_Microservice/src/Shared/Services/api.service.ts
@@ -1,12 +1,12 @@
-import axios, { AxiosResponse } from 'axios';
+import got from 'got';
 
 export class ApiService {
 
-    public static getUserOrders(userName: string): Promise<AxiosResponse> {
-        return axios.get(`http://order-microservice:${process.env.ORDER_MICROSERVICE_PORT}/order/${userName}`);
+    public static getUserOrders(username: string): any {
+        return got(`${process.env.ORDER_MICROSERVICE_URL}/order/${username}`)
     }
 
-    public static getUser(username: string): Promise<AxiosResponse> {
-        return axios.get(`http://user-microservice:${process.env.USER_MICROSERVICE_PORT}/user/${username}`);
+    public static getUser(username: string): any {
+        return got(`${process.env.USER_MICROSERVICE_URL}/user/${username}`);
     }
 }
